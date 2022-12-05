a22df39b351a7f18d1468f9a8e75f17b489affe1
manoj2852299 <mk2852299@gmail.com>
2022-12-05 22:36:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const { Socket } = require('socket.io');
+
+var express = require('express')
+const app = express(); 
+var http = require('http').Server(app); 
+var io = require('socket.io')(http);
+
+const port=3000
+
+app.use(express.static('public'))
+
+
+io.on("connection", (socket) => {
+
+    console.log('socket connected');
+
+    socket.emit("check", "connection successful")
+
+    socket.on("message", (data) => {
+
+        console.log(data);
+
+        socket.broadcast.emit("reply", data);
+
+    })
+
+
+})
+
+
+app.get('/person1', (req, res) => {
+
+    res.sendFile(__dirname + '/public/index1.html');
+
+
+})
+
+app.get('/person2', (req, res) => {
+
+    res.sendFile(__dirname + '/public/index2.html');
+
+
+})
+
+
+
+http.listen(port, () => {
+
+    console.log(`server listening on port ${port}`);
+});
